accc8eebd2d1a989009f21bc99302317f2a8a968
bw4sz <benweinstein2010@gmail.com>
2020-01-15 14:37:17 -0800

    is it pytest

--- test_dask.py
+++ test_dask.py
@@ -26,7 +26,7 @@ def add(x, y):
     return x + y
 
 client = start_dask_cluster(number_of_workers=2, mem_size="11GB")
-data = [1, 2, 3, 4, 5] * 10
+data = [1, 2, 3, 4, 5] * 100
 
 output = []
 for x in data:
