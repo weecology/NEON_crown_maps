7025aa1cef1764a97a2e047935f0ac7e3cdb7b68
bw4sz <benweinstein2010@gmail.com>
2020-01-15 12:33:13 -0800

    add scheduler test

---
+++ tests/test_dask.py
@@ -0,0 +1,43 @@
+from start_cluster import start_dask_cluster
+import pytest
+import dask
+import time
+import random
+
+@pytest.fixture
+def test_platform():
+    test_platform = platform.system()
+    return test_platform
+
+def test_start_dask_cluster(test_platform):
+    
+    if test_platform =="Darwin":
+        pass
+    else:  
+        client = start_dask_cluster(number_of_workers=2, mem_size="11GB")
+        inc = dask.delayed(inc)
+        dec = dask.delayed(dec)
+        add = dask.delayed(add)
+        
+        zs = []
+        for i in range(256):
+            x = inc(i)
+            y = dec(x)
+            z = add(x, y)
+            zs.append(z)
+            
+        zs = dask.persist(*zs)
+        assert len(zs) == 256
+        assert zs[0] == 0
+
+def inc(x):
+    time.sleep(random.random())
+    return x + 1
+
+def dec(x):
+    time.sleep(random.random())
+    return x - 1
+    
+def add(x, y):
+    time.sleep(random.random())
+    return x + y
