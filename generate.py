cbf84096f08fd71927a62f55103fc8dd5244a15a
bw4sz <benweinstein2010@gmail.com>
2020-01-18 15:21:35 -0800

    generate crops

--- generate.py
+++ generate.py
@@ -4,14 +4,16 @@ import tfrecords
 import glob
 
 def find_files():
-    tile_list = glob.glob("tests/data/*.tif")
+    #tile_list = glob.glob("tests/data/*.tif")
+    tile_list = glob.glob("./**/*image.tif",recursive=True)
     return tile_list
 
 #Start SLURM cluster
-client = start_dask_cluster(number_of_workers=2)
+client = start_dask_cluster(number_of_workers=100)
 
 #Find files
 tile_list = find_files()
 
 written_records = client.map(tfrecords.create_tfrecords, tile_list, patch_size=800, savedir="/orange/ewhite/b.weinstein/NEON/crops/")
+wait(written_records)
 print("{} records created".format(len(written_records)))
