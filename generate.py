827f6a1b1ee85ade21a51a64dcc80d4a004095d0
bw4sz <benweinstein2010@gmail.com>
2020-01-18 15:06:17 -0800

    generate crops

--- generate.py
+++ generate.py
@@ -13,5 +13,5 @@ client = start_dask_cluster(number_of_workers=2)
 #Find files
 tile_list = find_files()
 
-written_records = client.map(tile_list, tfrecords.create_tfrecords, patch_size=800, savedir="/orange/ewhite/b.weinstein/NEON/crops/")
+written_records = client.map(tfrecords.create_tfrecords, tile_list, patch_size=800, savedir="/orange/ewhite/b.weinstein/NEON/crops/")
 print("{} records created".format(len(written_records)))
