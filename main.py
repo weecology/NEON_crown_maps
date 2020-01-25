969416669f9fe74a9c6b235b339cf203988de04c
bw4sz <benweinstein2010@gmail.com>
2020-01-25 08:50:47 -0800

    log metrics

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from comet_ml import Experiment
 import glob
 import predict
 import os
 
+comet_experiment = Experiment(api_key="ypQZhYfs3nSyKzOfz13iuJpj2",
+                              project_name="frenchguiana", workspace="bw4sz")
+ 
 #RGB files
 rgb_list = glob.glob("/orange/ewhite/NeonData/**/*image.tif",recursive=True)
 
@@ -16,12 +20,15 @@ indices = [rgb_names.index(x) for x in tfrecord_names]
 raster_dir = [rgb_list[x] for x in indices]
 raster_dir = [os.path.dirname(x) for x in raster_dir]
 
+comet_experiment.log_parameter("Number of tiles",len(tfrecord_list))
 records = tfrecord_list[:1]
 
 #Create model and set config
 model = predict.create_model()
 print(model.config)
 #model.config["multi-gpu"] = 2
+model.config["batch_size"] = 6 
 
 #Predict
-predict.predict_tiles(model, records, patch_size=800, raster_dir=raster_dir, save_dir="/orange/ewhite/b.weinstein/NEON/predictions/", batch_size=6)
+comet_experiment.log_parameters(model.config)
+predict.predict_tiles(model, records, patch_size=800, raster_dir=raster_dir, save_dir="/orange/ewhite/b.weinstein/NEON/predictions/", batch_size=model.config["batch_size"])
