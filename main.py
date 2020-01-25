36ce63c704cac47ca1059d62dc53b2874fe5fa15
bw4sz <benweinstein2010@gmail.com>
2020-01-24 16:48:21 -0800

    typo

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ indices = [rgb_names.index(x) for x in tfrecord_names]
 raster_dir = [rgb_list[x] for x in indices]
 raster_dir = [os.path.dirname(x) for x in raster_dir]
 
-records = records[:1]
+records = tfrecord_list[:1]
 
 #Create model and set config
 model = predict.create_model()
