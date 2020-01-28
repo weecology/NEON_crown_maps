fa317df368e5d7245bfa41f3b56a9be7a97293e1
bw4sz <benweinstein2010@gmail.com>
2020-01-28 00:08:38 -0500

    single gpu profile

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ raster_dir = [rgb_list[x] for x in indices]
 raster_dir = [os.path.dirname(x) for x in raster_dir]
 
 comet_experiment.log_parameter("Number of tiles",len(tfrecord_list))
-records = tfrecord_list[:1]
+records = tfrecord_list[:10]
 
 #Create model and set config
 model = predict.create_model()
 print(model.config)
 model.config["multi-gpu"] = 1
-model.config["batch_size"] = 1 
+model.config["batch_size"] = 25
 
 #Predict
 comet_experiment.log_parameters(model.config)
-predict.predict_tiles(model, records, patch_size=800, raster_dir=raster_dir, save_dir="/orange/ewhite/b.weinstein/NEON/predictions/", batch_size=model.config["batch_size"])
+predict.predict_tiles(model, records, patch_size=400, raster_dir=raster_dir, save_dir="/orange/ewhite/b.weinstein/NEON/predictions/", batch_size=model.config["batch_size"])
