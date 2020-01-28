9f65279c479ce6415f243670b919e1a8e4f4d099
bw4sz <benweinstein2010@gmail.com>
2020-01-27 18:00:26 -0800

    try a random 50

--- generate.py
+++ generate.py
@@ -3,6 +3,7 @@ from start_cluster import start_dask_cluster
 from dask.distributed import wait
 from utils import tfrecords
 import glob
+import random
 
 def find_files():
     #tile_list = glob.glob("tests/data/*.tif")
@@ -10,10 +11,12 @@ def find_files():
     return tile_list
 
 #Start SLURM cluster
-client = start_dask_cluster(number_of_workers=150, mem_size="7GB")
+client = start_dask_cluster(number_of_workers=50, mem_size="7GB")
 
 #Find files
 tile_list = find_files()
+random.shuffle(tile_list)
+tile_list = tile_list[:50]
 
 written_records = client.map(tfrecords.create_tfrecords, tile_list, patch_size=400, savedir="/orange/ewhite/b.weinstein/NEON/crops/")
 wait(written_records)
