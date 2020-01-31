875b73c3ed207f797f024116c928450e2389305c
bw4sz <benweinstein2010@gmail.com>
2020-01-31 12:04:47 -0800

    window transform complete

--- hard_mining.py
+++ hard_mining.py
@@ -2,7 +2,12 @@
 import os
 import geopandas
 import rasterio
+from rasterio import windows as rwindow
+from rasterio import mask
 import cv2
+from PIL import Image
+import numpy as np
+from shapely.geometry import box
 import pandas as pd
 
 from deepforest import preprocess
@@ -19,7 +24,7 @@ def run(image_path,prediction_path, save_dir=".", patch_size=400,patch_overlap=0
     
     cell_size = raster.res[0]
     
-    utm_xmin, utm_ymin, utm_xmax, utm_ymax = raster.bounds
+    left, bottom, right, top = raster.bounds
     #Create windows
     windows = preprocess.compute_windows(raster, patch_size=patch_size,patch_overlap=patch_overlap)
     
@@ -27,32 +32,54 @@ def run(image_path,prediction_path, save_dir=".", patch_size=400,patch_overlap=0
     #Score dict
     scores = {}
     data = {}
+    crop_index = {}
+    
+    #Create spatial index
+    spatial_index = shp.sindex
+    
     for index,window in enumerate(windows):
         window_xmin, window_ymin, width, height = window.getRect()
-        
+        print(index)
         #transform    
-        xmin = (window_xmin * cell_size) + utm_xmin
-        xmax = (window_xmin + width) * cell_size  + utm_xmin
-        ymin = (window_ymin) * cell_size  + utm_ymin
-        ymax = (window_ymin + height) * cell_size  + utm_ymin
+        xmin = (window_xmin * cell_size) + left
+        xmax = (window_xmin + width) * cell_size  + left
+        ymin = top  - (window_ymin * cell_size)  
+        ymax = top - (window_ymin - height) * cell_size
         
-        #Spatial clip to window
-        filtered_boxes = shp.cx[xmin:xmax, ymin:ymax]
+        #Spatial clip to window using spatial index for faster querying
+        possible_matches_index = list(spatial_index.intersection([xmin,ymin,xmax,ymax]))
+        possible_matches = shp.iloc[possible_matches_index]
+        
+        #quick pass, some edges overlap
+        try:
+            filtered_boxes = possible_matches.cx[xmin:xmax,ymin:ymax]
+        except:
+                continue
         scores[index] = pd.to_numeric(filtered_boxes.score).mean()
         data[index] = filtered_boxes
+        crop_index[index] = window
     
     #Find highest 5th and lowest fifth quartile
-    score_df = pd.DataFrame(scores)
-    lowest = score_df.quantile(q=0.05)
-    worst_windows = data[lowest.index]
-    
-    highest = score_df.quantile(q=0.95)
-    best_windows = data[highest.index]
+    score_df = pd.Series(scores)
+    lowest = score_df.min()
+    lowest_index = score_df[score_df == lowest].index[0]
+    worst_window = data[lowest_index]
     
     #save RGB windows to file
-    for index in highest.index:
-        crop = raster[windows[index].indices()]
-        crop_filename = os.path.join(save_dir,"{}.tif".format(image_name))
-        cv2.imwrite(crop_filename, crop)
+    rasterio_window = rwindow.Window.from_slices(rows=crop_index[lowest_index].indices()[0],cols=crop_index[lowest_index].indices()[1])    
+    crop_filename = os.path.join(save_dir,"{}_{}.tif".format(image_name,lowest_index)) 
+    rwindow.transform(rasterio_window,raster.window_transform)
+    
+    with  rasterio.open(crop_filename, 'w',dtype=out_profile["dtype"],
+                        driver="GTiff",
+                        count=3,
+                        crs=raster.crs,
+                        width=rasterio_window.width,
+                        transform=raster.window_transform(rasterio_window),
+                        height=rasterio_window.height) as dst:
+        RP1_block = raster.read(window=rasterio_window, masked=True)  
+        dst.write(RP1_block)
         
     #Format annotations frame
+    shp_filename = os.path.join(save_dir,"{}_{}.shp".format(image_name,lowest_index))    
+    worst_window.to_file(shp_filename, driver='ESRI Shapefile')
