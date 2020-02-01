5c88c427119f761219de81b3950d981fce315049
bw4sz <benweinstein2010@gmail.com>
2020-01-31 16:14:52 -0800

    hipergator tests for mined images

--- tests/test_hard_mining.py
+++ tests/test_hard_mining.py
@@ -2,6 +2,6 @@
 from .. import hard_mining
 
 def test_run():
-    prediction_path = "/Users/ben/Downloads/predictions/2018_BART_4_318000_4876000_image.shp"
-    image_path = "/Users/ben/Downloads/predictions/2018_BART_4_318000_4876000_image.tif"
+    prediction_path = "/Users/ben/Documents/NEON_crown_maps/tests/2019_SOAP_4_306000_4099000_image.shp"
+    image_path = "/Users/ben/Documents/NEON_crown_maps/tests/2019_SOAP_4_306000_4099000_image.tif"
     hard_mining.run(prediction_path = prediction_path, image_path=image_path)
