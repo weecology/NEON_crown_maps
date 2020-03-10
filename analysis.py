f49b11bcdf61e1646fdaa1e567a57b1f9b255593
bw4sz <benweinstein2010@gmail.com>
2020-03-10 09:36:17 -0700

    missing arg

--- analysis.py
+++ analysis.py
@@ -4,6 +4,7 @@ import geopandas
 import numpy as np
 from geopandas.tools import sjoin
 import rtree
+import re
 from matplotlib import pyplot
 
 def match_years(geo_index, shps):
@@ -15,7 +16,13 @@ def match_years(geo_index, shps):
     #Load shapefiles
     shapefiles = [ ]
     for shp in matched_shps:
-        shapefiles.append(geopandas.read_file(shp))
+        #Load data and give it site and year and tile labels
+        df = geopandas.read_file(shp)
+        geo_index = re.search("(\d+_\d+)_image",shp).group(1)
+        df["geo_index"] = geo_index
+        df["Year"] = re.search("(\d+)_(\w+)_\d_\d+_\d+_image.shp",shp).group(1)
+        df["Site"] = re.search("(\d+)_(\w+)_\d_\d+_\d+_image.shp",shp).group(2)
+        shapefiles.append(df)
     
     #Join features and create a blank IoU column
     joined_boxes = sjoin(shapefiles[0],shapefiles[1])
@@ -47,7 +54,6 @@ def match_years(geo_index, shps):
         #Update value
         joined_boxes.loc[(joined_boxes.index == target_index) & (joined_boxes.index_right == matched_index),"IoU"] = iou_list[max_overlap_index]
         
-        
     #remove trees with less than threshold IoU
     threshold_boxes = joined_boxes[joined_boxes.IoU > 0.9]
     
@@ -55,7 +61,6 @@ def match_years(geo_index, shps):
     threshold_boxes["Height_difference"] = threshold_boxes["height_right"] - threshold_boxes["height_left"]
     
     #remove outliers
-    
     lower, upper = threshold_boxes.Height_difference.quantile([0.001,0.999]).values
     threshold_boxes = threshold_boxes[(threshold_boxes.Height_difference > lower) & (threshold_boxes.Height_difference < upper)]
     threshold_boxes.Height_difference.hist(bins=20)
@@ -64,4 +69,5 @@ def match_years(geo_index, shps):
     print("Median height difference is {}".format(threshold_boxes.Height_difference.median()))
     
     pyplot.savefig("tree_height.png")
+    return threshold_boxes
         
