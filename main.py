f39b5e436cfccbd14e0d0ede72a3b6a8a4191547
bw4sz <benweinstein2010@gmail.com>
2020-03-13 12:51:29 -0400

    merge

--- main.py
+++ main.py
@@ -125,9 +125,9 @@ def run_lidar(shp, CHM_path, min_height=3, save_dir=""):
 if __name__ == "__main__":
     
     #Create dask clusters
-    cpu_client = start(cpus = 15, mem_size ="11GB")
+    cpu_client = start(cpus = 30, mem_size ="11GB")
     
-    gpu_client = start(gpus=5,mem_size ="15GB")
+    gpu_client = start(gpus=8,mem_size ="15GB")
     
     #File lists
     rgb_list = glob.glob("/orange/ewhite/NeonData/**/Mosaic/*image.tif",recursive=True)
@@ -152,9 +152,9 @@ if __name__ == "__main__":
     #"2019_BONA_3_476000_7233000_image.tif"]
     
     target_list = None
-    site_list = ["BART","TEAK"]
+    site_list = ["OSBS"]
     year_list = ["2019","2018","2017"]
-    generated_records = generate_tfrecord(rgb_list, cpu_client, n=10, target_list = target_list, site_list=site_list, year_list=year_list)
+    generated_records = generate_tfrecord(rgb_list, cpu_client, n=None, target_list = target_list, site_list=site_list, year_list=year_list)
     
     predictions = []    
     
