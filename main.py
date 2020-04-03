4e8a6513f72c69889a405b2389c67a41ee7cf210
bw4sz <benweinstein2010@gmail.com>
2020-04-03 10:10:53 -0700

    try manually exporting tmpdir

--- main.py
+++ main.py
@@ -201,8 +201,8 @@ if __name__ == "__main__":
     target_list = None
     site_list = ["ABBY","ARIK","BARR","BART","BLAN","BONA","CLBJ","CPER","CUPE","DEJU","DELA","DSNY","GRSM","GUAN",
     "GUIL","HARV","HEAL","HOPB","JERC","JORN","KONZ","LAJA","LENO","LIRO","MCDI","MLBS","MOAB","NIWO","NOGP","OAES","OSBS","PRIN","PUUM","REDB","RMNP","SCBI","SERC","SJER","SOAP","SRER","STEI","STER","TALL","TEAK","TOOL","UKFS","UNDE","WLOU","WOOD","WREF","YELL"]
-    year_list = None
     #site_list = ["OSBS","DELA","BART","TEAK","BONA","SOAP","WREF"]
+    #year_list = None    
     year_list = ["2019","2018"]
     generated_records = generate_tfrecord(tile_list=rgb_list,
                                           lidar_pool=lidar_list,
