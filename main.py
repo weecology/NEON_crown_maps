53404ce20b36b3d04fb0c6acda53e2b29a060c50
bw4sz <benweinstein2010@gmail.com>
2020-04-03 09:50:26 -0700

    cannot find error on two sites, confirming still on full sites

--- main.py
+++ main.py
@@ -202,9 +202,7 @@ if __name__ == "__main__":
     site_list = ["ABBY","ARIK","BARR","BART","BLAN","BONA","CLBJ","CPER","CUPE","DEJU","DELA","DSNY","GRSM","GUAN",
     "GUIL","HARV","HEAL","HOPB","JERC","JORN","KONZ","LAJA","LENO","LIRO","MCDI","MLBS","MOAB","NIWO","NOGP","OAES","OSBS","PRIN","PUUM","REDB","RMNP","SCBI","SERC","SJER","SOAP","SRER","STEI","STER","TALL","TEAK","TOOL","UKFS","UNDE","WLOU","WOOD","WREF","YELL"]
     year_list = None
-    site_list = None
     #site_list = ["OSBS","DELA","BART","TEAK","BONA","SOAP","WREF"]
-    #site_list = ["DELA","DEJU"]
     year_list = ["2019","2018"]
     generated_records = generate_tfrecord(tile_list=rgb_list,
                                           lidar_pool=lidar_list,
