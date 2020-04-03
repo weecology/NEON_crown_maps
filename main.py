9fbbe0fa15b3965d25cef8614e7d647ee6ec04fa
bw4sz <benweinstein2010@gmail.com>
2020-04-03 14:38:07 -0700

    scaling up batching

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ if __name__ == "__main__":
    "2019_DELA_5_421000_3606000_image.tif",
     "2019_BONA_3_476000_7233000_image.tif"]
     
-    #target_list = None
+    target_list = None
     #site_list = ["ABBY","ARIK","BARR","BART","BLAN","BONA","CLBJ","CPER","CUPE","DEJU","DELA","DSNY","GRSM","GUAN",
     #"GUIL","HARV","HEAL","HOPB","JERC","JORN","KONZ","LAJA","LENO","LIRO","MCDI","MLBS","MOAB","NIWO","NOGP","OAES","OSBS","PRIN","PUUM","REDB","RMNP","SCBI","SERC","SJER","SOAP","SRER","STEI","STER","TALL","TEAK","TOOL","UKFS","UNDE","WLOU","WOOD","WREF","YELL"]
     site_list = ["OSBS","DELA","BART","TEAK","BONA","SOAP","WREF"]
