258429454bfb31655f12493d051b1c9df1ed94f6
bw4sz <benweinstein2010@gmail.com>
2020-06-10 12:45:07 -0700

    extract nlcds

--- App/setup.sh
+++ App/setup.sh
@@ -2,12 +2,6 @@
 ssh <>
 sudo docker pull visus/dataportal
 
-#Open port and run
-sudo docker run -it --rm -p 8080:80 visus/dataportal
-
-#persist data
-docker run -it --rm -p 8080:80 -v /my/data/folder:/data visus/dataportal
-
 #change config, see config.js
 #change internal server settings
-sudo docker run -it --rm -p 8080:80 -v /home/bweinstein/config.js:/home/OpenVisus/dataportal/viewer/config.js visus/dataportal
+sudo docker run -it --rm -p 8080:80 -v /home/bweinstein/config.js:/home/OpenVisus/dataportal/ext/visus/config.js visus/dataportal
---
+++ analysis/extract_nlcd.py
@@ -0,0 +1,60 @@
+import geopandas as gpd
+import pandas as pd
+import os
+import glob
+import rasterio
+import rasterstats 
+
+from scipy.stats import mode
+from shapely.geometry import Point
+from fiona.crs import from_epsg
+from ..crown_maps import start_cluster
+
+def get_epsg(site):
+    lookup = pd.read_csv("//orange/idtrees-collab/NLCD_2016/sites_utm_zone.csv")
+    id = lookup['siteID']==site
+    utmZone = lookup['utmZone'][id]
+    utmZone=utmZone.iat[0]
+    
+    return(utmZone)
+
+def create_point(x,y):
+    
+    return Point(x,y)
+
+def raster_mode(x):
+    """Get height quantile of all cells that are no zero"""
+    
+    return mode(x)
+
+def run(site_csv):
+    print(site_csv)
+    #read file
+    sitedf = pd.read_csv(site_csv)
+    
+    #Create point class 
+    sitedf["geometry"] = sitedf.apply(lambda x: create_point(x.plot_center_x, x.plot_center_y))
+    site = sitedf.site.unique()[0]
+    
+    #Get spatial projection
+    epsg = get_epsg(site)
+    geodf = gpd.GeoDataFrame(sitedf,geometry="geometry",crs=from_epsg(get_epsg(site)))
+    
+    #Load NLCD
+    nlcdpath = "/orange/idtrees-collab/NLCD_2016/NLCD_2016_Land_Cover_L48_20190424.img"
+    nlcd_array = rasterio.read(nlcdpath)
+    
+    #extract nlcd class
+    class_dict = rasterstats.zonal_stats(geodf, nlcd_array, add_stats={'mode':raster_mode})
+    sitedf["nlcd"]  = [g["mode"] for g in class_dict]
+    
+    #write alonside original
+    fn = "{}_nlcd.csv".format(os.path.splitext(site_csv))
+    sitedf.to_csv(fn)
+    
+if __name__ == "__main__":
+    #client = start_cluster.start(cpus=10)
+    file_list = glob.glob("/home/b.weinstein/NEON_crown_maps/Figures/sampling*.csv")
+    run(file_list[0])
+    #client.map(run, file_list)
+    
