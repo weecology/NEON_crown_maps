36690382314e09021e431dce6fcdce1355415c2f
bw4sz <benweinstein2010@gmail.com>
2020-06-10 16:31:39 -0400

    extrct returns 0

--- analysis/extract_nlcd.py
+++ analysis/extract_nlcd.py
@@ -8,7 +8,7 @@ import rasterstats
 from scipy.stats import mode
 from shapely.geometry import Point
 from fiona.crs import from_epsg
-from ..crown_maps import start_cluster
+from crown_maps import start_cluster
 
 def get_epsg(site):
     lookup = pd.read_csv("//orange/idtrees-collab/NLCD_2016/sites_utm_zone.csv")
@@ -25,7 +25,7 @@ def create_point(x,y):
 def raster_mode(x):
     """Get height quantile of all cells that are no zero"""
     
-    return mode(x)
+    return mode(x)[0][0]
 
 def run(site_csv):
     print(site_csv)
@@ -33,28 +33,28 @@ def run(site_csv):
     sitedf = pd.read_csv(site_csv)
     
     #Create point class 
-    sitedf["geometry"] = sitedf.apply(lambda x: create_point(x.plot_center_x, x.plot_center_y))
+    sitedf["geometry"] = sitedf.apply(lambda x: create_point(x.plot_center_x, x.plot_center_y),axis=1)
     site = sitedf.site.unique()[0]
     
     #Get spatial projection
     epsg = get_epsg(site)
     geodf = gpd.GeoDataFrame(sitedf,geometry="geometry",crs=from_epsg(get_epsg(site)))
-    
+    geodf['geometry'] = geodf.geometry.buffer(5)
+
     #Load NLCD
     nlcdpath = "/orange/idtrees-collab/NLCD_2016/NLCD_2016_Land_Cover_L48_20190424.img"
-    nlcd_array = rasterio.read(nlcdpath)
     
     #extract nlcd class
-    class_dict = rasterstats.zonal_stats(geodf, nlcd_array, add_stats={'mode':raster_mode})
+    class_dict = rasterstats.zonal_stats(geodf, nlcdpath, add_stats={'mode':raster_mode})
     sitedf["nlcd"]  = [g["mode"] for g in class_dict]
     
     #write alonside original
-    fn = "{}_nlcd.csv".format(os.path.splitext(site_csv))
+    fn = "{}_nlcd.csv".format(os.path.splitext(site_csv)[0])
     sitedf.to_csv(fn)
     
 if __name__ == "__main__":
     #client = start_cluster.start(cpus=10)
     file_list = glob.glob("/home/b.weinstein/NEON_crown_maps/Figures/sampling*.csv")
-    run(file_list[0])
+    run(file_list[10])
     #client.map(run, file_list)
     
