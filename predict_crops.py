88c1f2cd91860156a2eba90cc32cd97341c342db
bw4sz <benweinstein2010@gmail.com>
2020-11-07 17:33:44 -0800

    Scatter model load

--- predict_crops.py
+++ predict_crops.py
@@ -5,22 +5,21 @@ from crown_maps import start_cluster
 client = start_cluster.start(gpus=5)
 client.wait_for_workers(2)
 
-def run(x):
+def load_model():
     from deepforest import deepforest    
     saved_model="/home/b.weinstein/miniconda3/envs/DeepTreeAttention_DeepForest/lib/python3.7/site-packages/deepforest/data/NEON.h5"
     model = deepforest.deepforest(saved_model = saved_model)
     
-    results = []
+    return model
+
+def run(x, model):
     boxes = model.predict_image(x, return_plot = False)
     boxes["file"] = x
-    results.append(boxes)
-    
-    results = pd.concat(results)
-    return results
+    csv_name = "/orange/ewhite/b.weinstein/NeonTreeEvaluation/pretraining/predictions/{}.csv".format(x)
+    boxes.to_csv(csv_name)
+    return x
 
 files = glob.glob("/orange/ewhite/b.weinstein/NeonTreeEvaluation/pretraining/crops/*.jpg")
 futures = client.scatter(files)
-results = client.map(run, futures)
-full_set = client.gather(results)
-full_set = pd.concat(full_set)
-full_set.to_csv("predictions.csv")
+model_future = client.run(load_model)
+results = client.map(run, futures, model = model_future)
