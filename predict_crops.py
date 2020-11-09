a07ea12d17afcef3b7e364af4dddd2703d2bb6ef
bw4sz <benweinstein2010@gmail.com>
2020-11-09 08:50:14 -0800

    Scatter as numpy arrays

--- predict_crops.py
+++ predict_crops.py
@@ -1,25 +1,26 @@
 import pandas as pd
 import glob
+import numpy as np
 from crown_maps import start_cluster
 
 client = start_cluster.start(gpus=5)
 client.wait_for_workers(2)
 
-def load_model():
+def run(paths):
     from deepforest import deepforest    
     saved_model="/home/b.weinstein/miniconda3/envs/DeepTreeAttention_DeepForest/lib/python3.7/site-packages/deepforest/data/NEON.h5"
     model = deepforest.deepforest(saved_model = saved_model)
     
-    return model
-
-def run(x, model):
-    boxes = model.predict_image(x, return_plot = False)
-    boxes["file"] = x
-    csv_name = "/orange/ewhite/b.weinstein/NeonTreeEvaluation/pretraining/predictions/{}.csv".format(x)
-    boxes.to_csv(csv_name)
+    for x in paths:
+        boxes = model.predict_image(x, return_plot = False)
+        boxes["file"] = x
+        csv_name = "/orange/ewhite/b.weinstein/NeonTreeEvaluation/pretraining/predictions/{}.csv".format(x)
+        boxes.to_csv(csv_name)
     return x
 
 files = glob.glob("/orange/ewhite/b.weinstein/NeonTreeEvaluation/pretraining/crops/*.jpg")
-futures = client.scatter(files)
+chunks = np.array_split(np.array(files),5)
+futures = client.scatter(chunks)
 model_future = client.run(load_model)
 results = client.map(run, futures, model = model_future)
+client.gather(results)
