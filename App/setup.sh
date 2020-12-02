0209a20388776663ade7a06f0aa234da3702570c
Ben Weinstein <bw4sz@users.noreply.github.com>
2020-12-02 12:32:56 -0800

    Update setup.sh

--- App/setup.sh
+++ App/setup.sh
@@ -8,6 +8,8 @@ sudo azcopy login --tenant-id 6689f796-92e8-413e-864a-db37d7fca00b
 
 azcopy copy OpenVisus/ https://neon.blob.core.windows.net/openvisus --recursive
 
+#if this fails create a shared access token under the blob container 'sas' token must be a quotes.
+
 #change config, see config.js
 #change internal server settings
 sudo docker run -it --rm -p 8080:80 -v /home/bweinstein/config.js:/home/OpenVisus/dataportal/ext/visus/config.js visus/dataportal
