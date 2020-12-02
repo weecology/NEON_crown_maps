81ac383ef23a03c23984330c15efc22e02f8d7f1
Ben Weinstein <bw4sz@users.noreply.github.com>
2020-12-02 12:20:50 -0800

    Update setup.sh

--- App/setup.sh
+++ App/setup.sh
@@ -2,6 +2,12 @@
 ssh <>
 sudo docker pull visus/dataportal
 
+
+#Azcopy login
+sudo azcopy login --tenant-id 6689f796-92e8-413e-864a-db37d7fca00b
+
+azcopy copy OpenVisus/ https://neon.blob.core.windows.net/openvisus --recursive
+
 #change config, see config.js
 #change internal server settings
 sudo docker run -it --rm -p 8080:80 -v /home/bweinstein/config.js:/home/OpenVisus/dataportal/ext/visus/config.js visus/dataportal
