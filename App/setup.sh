167c34d1313281fa14644b51769f0310e03750c5
Ben Weinstein <bw4sz@users.noreply.github.com>
2020-12-04 14:10:48 -0800

    Update setup.sh

--- App/setup.sh
+++ App/setup.sh
@@ -12,4 +12,4 @@ azcopy copy OpenVisus/ https://neon.blob.core.windows.net/openvisus --recursive
 
 #change config, see config.js
 #change internal server settings
-sudo docker run -it --rm -p 8080:80 -v /home/bweinstein/config.js:/home/OpenVisus/dataportal/ext/visus/config.js visus/dataportal
+sudo docker run -it --rm -d -p 8080:80 -v /datadrive/OpenVisus/:/converted -v /home/bweinstein/NEON_crown_maps/App/config.js:/home/OpenVisus/dataportal/ext/visus/config.js visus/dataportal
